03461c79f76e81173fa2326380127370259df931
Elias20061 <156211431+Elias20061@users.noreply.github.com>
2024-02-24 11:50:41 +0100

    Add files via upload

---
+++ languages/events/fr.js
@@ -0,0 +1,30 @@
+module.exports = {
+	// You can customize the language here or directly in the command files
+	autoUpdateThreadInfo: {},
+	checkwarn: {
+		text: {
+			warn: "Le membre %1 a déjà été averti 3 fois et a été banni de la boîte de discussion\n- Nom : %1\n- Uid : %2\n- Pour débannir, veuillez utiliser \"%3warn unban <uid>\"  commande (avec uid est l'uid de la personne que vous souhaitez débannir)",
+ needPermission: "Le robot a besoin de l'autorisation de l'administrateur pour expulser les membres bannis"
+		}
+	},
+	leave: {
+		text: {
+			leaveType1: "nous a quitté 🙀, mais qu'est-ce qu'il n'a pas trouvé de bon dans ce groupe ?😞",
+			leaveType2: "à été supprimé du groupe par Un administrateur de ce groupe 👮🏾‍♂️, soit il a commit quelque chose de grave 🤷🏾‍♂️ "
+		}
+	},
+	logsbot: {
+		text: {
+			title: "====== Journaux du robot ======",
+ added: "\n✅\nÉvénement : le bot a été ajouté à un nouveau groupe\n- Ajouté par : %1",
+ kicked: "\n❌\nÉvénement : le bot a été supprimé 😞\n- supprimé par : %1",
+ footer: "\n- ID utilisateur : %1\n- Groupe : %2\n- ID de groupe : %3\n- Heure : %4"
+		}
+	},
+	onEvent: {},
+	welcome: {
+		text: {
+			welcomeMessage: "𝖩𝖾 𝖵𝗈𝗎𝗌  𝗋𝖾𝗆𝖾𝗋𝖼𝗂𝖾 𝖽𝖾 𝗆'𝖺𝗏𝗈𝗂𝗋 𝖺𝗃𝗈𝗎𝗍é 𝖽𝖺𝗇𝗌 𝖼𝖾 𝗀𝗋𝗈𝗎𝗉𝖾 😃, 𝗃𝖾 𝗆𝖾 𝗉𝗋é𝗌𝖾𝗇𝗍𝖾 : 𝗆𝗈𝗇 𝗇𝗈𝗆 𝖤𝗌𝗍 𝖤𝗋𝗐𝗂𝗇 𝖲𝗆𝗂𝗍𝗁 𝗃𝖾 𝗌𝗎𝗂𝗌 𝗎𝗇 𝖻𝗈𝗍 𝖼𝗋éé👨🏾‍🔧 𝗉𝖺𝗋 𝖤𝖫𝖨𝖠𝖲 𝖡𝖠𝖱𝖴𝖳𝖨 :https://www.facebook.com/elias.baruti45\n𝖬𝗈𝗇 𝗉𝗋é𝖿𝗂𝗑 𝖾𝗌𝗍 : %1\n𝖯𝗈𝗎𝗋  𝗏𝗈𝗂𝗋 𝗍𝗈𝗎𝗍𝖾𝗌 𝗆𝖾𝗌 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝖾𝗌 ,𝗌𝗏𝗉 𝗏𝖾𝗎𝗂𝗅𝗅𝖾𝗓 𝗍𝖺𝗉𝖾𝗋 :%1help ",		
+		}
+	}
+};
